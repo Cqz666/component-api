53ccbcaf2d6887b5a4cef6eeb6138c98d1a053f3
chenqizhu <chenqizhu@4399inc.com>
2022-01-26 22:07:02 +0800

    rdd

---
+++ common-scala/src/main/scala/com/cqz/rdd/SparkFileRdd.scala
@@ -0,0 +1,17 @@
+package com.cqz.rdd
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkFileRdd {
+  def main(args: Array[String]): Unit = {
+    val sparkconf = new SparkConf().setMaster("local[*]").setAppName("a")
+    val sc = new SparkContext(sparkconf)
+
+    val seq: Seq[Int] = Seq(1,2,3,4)
+    val rdd: RDD[String] = sc.textFile("data/*")
+    rdd.collect().foreach(println)
+
+    sc.stop()
+  }
+}
