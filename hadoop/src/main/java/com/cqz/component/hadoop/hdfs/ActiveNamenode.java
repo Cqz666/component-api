5e8409da36d7b145e6ac90ae037d160a0556c975
chenqizhu <chenqizhu@4399inc.com>
2022-02-07 14:00:16 +0800

    tmp

--- hadoop/src/main/java/com/cqz/component/hadoop/hdfs/ActiveNamenode.java
+++ hadoop/src/main/java/com/cqz/component/hadoop/hdfs/ActiveNamenode.java
@@ -1,5 +1,6 @@
 package com.cqz.component.hadoop.hdfs;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.hdfs.HAUtil;
 
@@ -7,17 +8,31 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 import static com.cqz.component.hadoop.hdfs.FtpToHdfs.getHdfsFileSystem;
+import static com.cqz.component.hadoop.hdfs.HdfsTool.getFileSystem;
+import static com.cqz.component.hadoop.hdfs.HdfsUtil.getHadoopConf;
 
 public class ActiveNamenode {
 
     public static void main(String[] args) throws IOException {
-        FileSystem fs = getHdfsFileSystem();
+        FileSystem fs = getHdfsFileSystem(args[0]);
         InetSocketAddress addressOfActive = HAUtil.getAddressOfActive(fs);
         String hostString = addressOfActive.getHostString();
         String hostName = addressOfActive.getHostName();
         int port = addressOfActive.getPort();
-        System.out.println("hostString = "+hostString);
+        System.out.println("host = "+hostString);
         System.out.println("hostName:port = "+hostName+":"+port);
 
+    }
+
+    public static FileSystem getHdfsFileSystem(String conf){
+        Configuration hadoopConf = getHadoopConf(conf);
+        hadoopConf.set("fs.defaultFS","hdfs://flinkcluster");
+        FileSystem fileSystem=null;
+        try {
+            fileSystem = getFileSystem(hadoopConf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileSystem;
     }
 }
