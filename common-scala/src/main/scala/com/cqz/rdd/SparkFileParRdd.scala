abe95e5f14fadb13c5b5da9316dd6c539f7314ff
chenqizhu <chenqizhu@4399inc.com>
2022-02-08 21:59:23 +0800

    spark practice

---
+++ common-scala/src/main/scala/com/cqz/rdd/SparkFileParRdd.scala
@@ -0,0 +1,17 @@
+package com.cqz.rdd
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkFileParRdd {
+  def main(args: Array[String]): Unit = {
+    val sparkconf = new SparkConf().setMaster("local[*]").setAppName("a")
+    val sc = new SparkContext(sparkconf)
+
+    val rdd: RDD[String] = sc.textFile("data/3.txt",2)
+
+    rdd.saveAsTextFile("output")
+
+    sc.stop()
+  }
+}
